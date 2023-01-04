5667e02a2d284e96b7d922969aaae953c044f672
FangYuan <374072213@qq.com>
2023-01-04 21:08:25 +0800

    调用tts dubbo接口完毕

--- learn-dubbo-demo/dubbo-consumer/src/main/java/com/dubbo/learn/consumer/ConsumerApplication.java
+++ learn-dubbo-demo/dubbo-consumer/src/main/java/com/dubbo/learn/consumer/ConsumerApplication.java
@@ -1,6 +1,8 @@
 package com.dubbo.learn.consumer;
 
 import com.dubbo.learn.DemoService;
+import com.tts.remote.dto.IovConfigDto;
+import com.tts.remote.service.SystemRemoteService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
@@ -15,13 +17,27 @@ public class ConsumerApplication {
 
     @DubboReference
     private DemoService demoService;
+    @DubboReference
+    private SystemRemoteService systemRemoteService;
 
     public static void main(String[] args) {
-
         ConfigurableApplicationContext context = SpringApplication.run(ConsumerApplication.class, args);
         ConsumerApplication application = context.getBean(ConsumerApplication.class);
-        String result = application.doSayHello("world");
-        System.out.println("result: " + result);
+        String content = "{\n" +
+                "    \"accessId\": \"123\",\n" +
+                "    \"secretKey\": \"123\",\n" +
+                "    \"baseUrl\": \"openapi.huoyunren.com\"\n" +
+                "}";
+        IovConfigDto iovConfigDto = new IovConfigDto();
+
+        iovConfigDto.setIovType("G7");
+        iovConfigDto.setConfigInfo(content);
+
+        System.out.println(application.doInitialIovConfig(iovConfigDto));
+    }
+
+    public boolean doInitialIovConfig(IovConfigDto iovConfigDto) {
+        return systemRemoteService.saveOrUpdateIovConfig(iovConfigDto);
     }
 
     public String doSayHello(String name) {
--- learn-dubbo-demo/dubbo-provider/src/main/java/com/dubbo/learn/provider/ProviderApplication.java
+++ learn-dubbo-demo/dubbo-provider/src/main/java/com/dubbo/learn/provider/ProviderApplication.java
@@ -11,8 +11,6 @@ import java.util.concurrent.CountDownLatch;
 @EnableDubbo
 public class ProviderApplication {
     public static void main(String[] args) throws Exception {
-        new EmbeddedZooKeeper(2181, false).start();
-
         SpringApplication.run(ProviderApplication.class, args);
         System.out.println("dubbo service started");
         new CountDownLatch(1).await();
